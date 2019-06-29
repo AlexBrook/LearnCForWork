51fa906a5173bff1dc54b71566ae01102dbe560d
Alex Brook <testerlearning12@gmail.com>
2019-06-29 18:57:55 +0300

    Добавьте файлы проекта.

---
+++ TreehouseDefense/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreehouseDefense
+{
+    class Game
+    {
+        public static void Main() {
+            Tower tower = new Tower();
+            Map map = new Map(8,5);
+
+           int area = map.width * map.height;
+
+        }
+    }
+}
---
+++ TreehouseDefense/Class3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreehouseDefense
+{
+    class Map
+    {
+       public readonly int width;
+       public readonly int height;
+
+       public Map(int Width, int Height) {
+            Width = width;
+            Height = height;
+        }
+    }
+}
